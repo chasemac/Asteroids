0c3f0be335bdb1ae1414a3dbee8ffd6f1570a452
Chase McElroy <ichasem@gmail.com>
2017-05-11 16:32:59 -0700

    fixed spelling, created Asteroid Behavior class. Programmatically added astroid views to VC

---
+++ Asteroids/AsteroidBehavior.swift
@@ -0,0 +1,13 @@
+//
+//  AsteroidBehavior.swift
+//  Asteroids
+//
+//  Created by Chase McElroy on 5/11/17.
+//  Copyright © 2017 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class AsteroidBehavior: UIDynamicBehavior {
+
+}
